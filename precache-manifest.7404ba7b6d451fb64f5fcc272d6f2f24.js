198e1e3b8c30f1b69a9f4cb8d877d52097bdc583
Faeez <faeez.hartley@gmail.com>
2020-04-24 17:07:45 +0200

    Updates

---
+++ precache-manifest.7404ba7b6d451fb64f5fcc272d6f2f24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "466c9bdc1c6b6cd9a07b482c3694554c",
+    "url": "/Gfinder/index.html"
+  },
+  {
+    "revision": "bf9f11cc8e0ff3aef5df",
+    "url": "/Gfinder/static/css/main.39491247.chunk.css"
+  },
+  {
+    "revision": "30540f98817407b0dafd",
+    "url": "/Gfinder/static/js/2.21d588ef.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Gfinder/static/js/2.21d588ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf9f11cc8e0ff3aef5df",
+    "url": "/Gfinder/static/js/main.5e011d2d.chunk.js"
+  },
+  {
+    "revision": "328815f102400307f0f2",
+    "url": "/Gfinder/static/js/runtime-main.49d2cad7.js"
+  }
+]);
